8581102f6e4fecfc8d231e6a3b12779beab49806
OchoKOM <118879243+OchoKOM@users.noreply.github.com>
2023-06-23 16:42:08 +0100

    Delete Version 1 directory

--- Version 1/php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-try{
-    $bdd = new PDO('mysql:host=localhost;dbname=video;charset=utf8', 'root','');
-} catch(Exception $e){
-    die('Une erreur a été trouvé : ' . $e->getMessage()); 
-}
